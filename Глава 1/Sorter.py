7a63f3457827c08e0e5a32388932b6bd52594264
Pycat12 <110453377+Pycat12@users.noreply.github.com>
2022-08-03 18:07:26 +0300

    Add files via upload

---
+++ Глава 1/Sorter.py
@@ -0,0 +1,15 @@
+def find_Smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = [0]
+    for i in range(len(arr)):
+        smallest = find_Smallest(arr)
+        newArr.append(arr.pop(smallest))
+        return newArr
+print(selectionSort([5, 3, 6, 2, 10]))
